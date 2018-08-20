eb8204a119f09de955ca48c43ba7ef724124bf60
thomashillenbrand <tomhillenbrand16@gmail.com>
2018-08-20 19:30:37 -0400

    Finished Week3 challenges

---
+++ week2_algorithmic_warmup/4_least_common_multiple/LCMFast.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class LCMFast {
+    private static long lcm(int a, int b) {
+        long lcm, gcd;
+        gcd = gcd(a, b);
+        lcm = a / gcd;
+        lcm = lcm * b;
+        return lcm;
+
+    }
+
+    private static int gcd(int a, int b) {
+        int current_gcd = 1;
+        int big, small, remainder, quotient;
+        if (a > b) {
+            big = a;
+            small = b;
+        } else {
+            big = b;
+            small = a;
+        }
+
+        do {
+            remainder = big % small;
+            current_gcd = small;
+            big = small;
+            small = remainder;
+
+        } while (remainder != 0);
+
+
+        return current_gcd;
+    }
+
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(lcm(a, b));
+    }
+}
